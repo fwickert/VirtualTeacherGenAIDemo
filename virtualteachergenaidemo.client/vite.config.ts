5886a574d56452e24fbbfea69c49a4fd09dad1e0
Frederic Wickert <fwickert@users.noreply.github.com>
2024-11-19 22:29:52 +0100

    Refactor launchSettings and Vite config for improved URL handling and consistency

--- virtualteachergenaidemo.client/vite.config.ts
+++ virtualteachergenaidemo.client/vite.config.ts
@@ -30,10 +30,9 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     }
 }
 
-const target = process.env.ASPNETCORE_URLS ? process.env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7273';
-console.log(`Target URL: ${target}`);
-const hubUrl = target + '/messageRelayHub'; 
+const target = env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7273';
 
+const hubUrl = `${target}/messageRelayHub`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -57,6 +56,8 @@ export default defineConfig({
         }       
     },
     define: {
-        "process.env.HUB_URL": JSON.stringify(hubUrl)
-    }
+        'process.env': {
+          HUB_URL: hubUrl
+        }
+      }
 })
