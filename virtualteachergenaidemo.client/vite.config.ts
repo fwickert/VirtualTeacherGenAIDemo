9562e349204da9273123de9784299cccc0f71d3a
Frederic Wickert <fwickert@users.noreply.github.com>
2024-11-19 21:37:59 +0100

    Refactor Vite config to use process.env for target URL assignment

--- virtualteachergenaidemo.client/vite.config.ts
+++ virtualteachergenaidemo.client/vite.config.ts
@@ -30,8 +30,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7273';
+const target = process.env.ASPNETCORE_URLS ? process.env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7273';
 
 const hubUrl = target + '/messageRelayHub'; 
 
