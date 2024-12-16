2e27fe4017b312d23dea3332f3907151b90d3b5c
Lucile Jeanneret <lujeanne@microsoft.com>
2024-12-16 22:05:30 +0100

    fix(iac)

--- deploy/main.tf
+++ deploy/main.tf
@@ -8,22 +8,20 @@ data "azurerm_resource_group" "rg" {
   name = "rg-virtual-teacher-demo-wus"
 }
 
-# Define the App Service Plan
 resource "azurerm_app_service_plan" "appserviceplan" {
   name                = "plan-virtual-teacher-demo-wus"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   sku {
     tier = "Standard"
     size = "S1"
   }
 }
 
-# Define the App Service
 resource "azurerm_app_service" "appservice" {
   name                = "webapp-virtual-teacher-demo-wus"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
 
   site_config {
