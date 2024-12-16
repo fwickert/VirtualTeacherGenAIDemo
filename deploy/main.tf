5fe77c792c00bb1c7b2dcb590f749315d9593803
Lucile Jeanneret <lujeanne@microsoft.com>
2024-12-16 22:51:35 +0100

    ci(workflows): use sp from secrets

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,6 +1,7 @@
 # Define the provider
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
 }
 
 # Reference to an existing resource group
@@ -23,8 +24,4 @@ resource "azurerm_app_service" "appservice" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-  }
 }
