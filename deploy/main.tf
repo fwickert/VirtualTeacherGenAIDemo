3d516b67e39d85a3da78cd3db83e79b1ed6b0319
Lucile Jeanneret <lujeanne@microsoft.com>
2024-12-17 14:41:56 +0100

    ci(webap): deploy to web app

--- deploy/main.tf
+++ deploy/main.tf
@@ -8,32 +8,28 @@ provider "azurerm" {
 data "azurerm_resource_group" "rg" {
   name = "rg-virtual-teacher-demo-wus"
 }
-# Create the Linux App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B1"
+
+# Reference to an existing App Service Plan
+data "azurerm_app_service_plan" "existing_plan" {
+  name                = "ASP-rgvirtualteacherdemowus-8ff3"
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
-  name                  = "webapp-${random_integer.ri.result}"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
-  https_only            = true
-  site_config { 
-    minimum_tls_version = "1.2"
+# Reference to an existing App Service (Web App)
+data "azurerm_app_service" "existing_webapp" {
+  name                = "webapp-virtual-teacher"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Deploy code to the existing Web App
+resource "azurerm_app_service_slot" "deployment_slot" {
+  name                = "staging"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  app_service_plan_id = data.azurerm_app_service_plan.existing_plan.id
+  app_service_name    = data.azurerm_app_service.existing_webapp.name
+
+  site_config {
+    scm_type = "LocalGit"
   }
-}
-
-#  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch             = "master"
-  use_manual_integration = true
-  use_mercurial      = false
 }
